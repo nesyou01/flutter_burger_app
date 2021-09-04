9900b98008b4c66f2a0c8be23b0fc1d6461d5635
Younes Lagmah <lagmahraja@gmail.com>
2021-09-04 15:59:02 +0100

    Add preview

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color kDarkColor = Color(0xFF1C1C1F);
+const Color kDarkColor = Color(0xFF222225);
 const Color kBlackColor = Color(0xFF19191D);
 const Color kYellowColor = Color(0xFFE8AF3D);
